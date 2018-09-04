2549af4ce2a8c839a002aae14d133e7a34453021
Mykhailo Paliukh <paliukh.mykhailo@gmail.com>
2018-09-04 14:57:17 +0300

    yarn.lock

--- index.js
+++ index.js
@@ -1,16 +1,24 @@
 'use strict';
 
-var path = require('path');
-var isValid = require('is-valid-app');
+const isValid = require('is-valid-app');
+const shell = require('shelljs');
 
 module.exports = function(app) {
   if (!isValid(app, 'capsula-ci-cd')) return;
   
-  app.task('travis', { silent: true }, function(cb) {
+  app.task('travis', { silent: true }, function() {
     return app.src('.travis.yml', {cwd: __dirname})
       .pipe(app.dest(function() {
         return app.cwd;
       }));
+  });
+  
+  app.task('deploy', function () {
+    shell.exec('./build/deploy')
+  });
+  
+  app.task('release', function () {
+    shell.exec('./build/release')
   });
   
   app.task('default', ['travis']);
