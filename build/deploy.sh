0eb149f1a487844813f05c09eb3fb14c2a8e8ec9
stephane Benayoun <stephane.benayoun@jivygroup.com>
2018-10-11 14:23:56 +0300

    Sync

--- build/deploy.sh
+++ build/deploy.sh
@@ -1,3 +1,12 @@
 #!/bin/bash
 
 echo "CapsulaJS: Deploying.."
+
+# install aws cli
+pip install --user awscli
+export PATH=$PATH:$HOME/.local/bin
+
+# upload to s3
+aws s3 rm $S3_PATH/$CURRENT_BRANCH_NAME --recursive --region $S3_REGION
+aws s3 cp dist $S3_PATH/$CURRENT_BRANCH_NAME --cache-control "max-age=$CACHE_CONTROL_MAX_AGE" --recursive
+aws cloudfront create-invalidation --distribution-id $CF_DISTRIBUTION_ID --paths "/$CURRENT_BRANCH_NAME/*"
